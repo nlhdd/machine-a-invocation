a1984b3f3d300e22d30584b50ccc7080eab49fd6
Nastassja <ndd@etik.com>
2026-06-27 19:12:10 +0200

    DOGGO SUMMONER IS READY
    Found doggo pixel art very cute
    changed theme of the project
    very better, very me.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const formulaire = document.getElementById("invocation");
+const resultat = document.getElementById("resultat");
+const doggo = document.getElementById("doggo");
+const magic = document.getElementById("magic");
+const againButton = document.getElementById("again");
 
 formulaire.addEventListener("submit", (event) => {
 	event.preventDefault();
@@ -7,16 +11,23 @@ formulaire.addEventListener("submit", (event) => {
 	const creature = document.getElementById("creature").value;
 
 	const message = `${prenom}, the ${creature} heard you and will follow you from now on!`;
-
 	document.getElementById("message").textContent = message;
 
-	document.getElementById("invocation").classList.add("cache");
-	document.getElementById("resultat").classList.remove("cache");
+	doggo.style.backgroundImage = `url('assets/${creature}-walk.png')`;
+	doggo.classList.add("cache");
+	magic.src = "assets/Dark-Bolt.gif?" + Date.now();
+	magic.classList.remove("cache");
+
+	setTimeout(() => {
+		magic.classList.add("cache");
+		doggo.classList.remove("cache");
+	}, 600);
+
+	formulaire.classList.add("cache");
+	resultat.classList.remove("cache");
 });
 
-const boutonAgain = document.getElementById("again");
-
-boutonAgain.addEventListener("click", () => {
-	document.getElementById("invocation").classList.remove("cache");
-	document.getElementById("resultat").classList.add("cache");
+againButton.addEventListener("click", () => {
+	formulaire.classList.remove("cache");
+	resultat.classList.add("cache");
 });
