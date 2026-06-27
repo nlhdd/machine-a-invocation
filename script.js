9b04b7afded20f9809a3bae712d7d417fa7e0ccb
Nastassja <ndd@etik.com>
2026-06-27 17:00:34 +0200

    Etape 4 -
    english version -
    js done

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ const formulaire = document.getElementById("invocation");
 
 formulaire.addEventListener("submit", (event) => {
 	event.preventDefault();
-	console.log("worked!");
+
+	const prenom = document.getElementById("prenom").value;
+	const creature = document.getElementById("creature").value;
+
+	const message = `${prenom}, the ${creature} heard you and will follow you from now on!`;
+
+	document.getElementById("message").textContent = message;
+
+	document.getElementById("invocation").classList.add("cache");
+	document.getElementById("resultat").classList.remove("cache");
 });
